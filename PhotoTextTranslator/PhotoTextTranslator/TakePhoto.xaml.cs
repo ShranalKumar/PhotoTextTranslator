78bce901d84b5f270d2541d015ff9682227c9bdd
Shranal Kumar <skmu316@aucklanduni.ac.nz>
2017-07-27 14:17:20 +1200

    OCR working

--- PhotoTextTranslator/PhotoTextTranslator/TakePhoto.xaml.cs
+++ PhotoTextTranslator/PhotoTextTranslator/TakePhoto.xaml.cs
@@ -1,9 +1,12 @@
-﻿using Plugin.Media;
+﻿using Newtonsoft.Json;
+using Plugin.Media;
 using Plugin.Media.Abstractions;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -22,6 +25,7 @@ namespace PhotoTextTranslator
 
         private async void Button_Clicked(object sender, EventArgs e)
         {
+            picSentence.Text = "";
             await CrossMedia.Current.Initialize();
 
             this.text = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
@@ -38,13 +42,56 @@ namespace PhotoTextTranslator
                     return this.text.GetStream();
                 });
             }
+            await GetText(text);
         }
 
         async Task GetText(MediaFile text)
         {
             var client = new HttpClient();
 
-            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "{subscription key}");
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "c42b1041a780481dbb9a828ff64d976b");
+
+            var uri = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/ocr?language=unk";
+
+            HttpResponseMessage response;
+
+            byte[] byteData = convertToByteArray(text);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(uri, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseString = await response.Content.ReadAsStringAsync();
+                    OCRModel model = JsonConvert.DeserializeObject<OCRModel>(responseString);
+
+                    List<regions> regions = model.regions;
+                    var sentence = "";
+
+                    foreach (regions r in regions )
+                    {
+                        List<lines> lines = r.lines;
+                        foreach (lines l in lines)
+                        {
+                            List<words> words = l.words;
+                            foreach(words w in words)
+                            {
+                                sentence += w.text + " ";
+                            }
+                        }
+                    }
+                    picSentence.Text = sentence;
+                }
+            }
+        }
+
+        static byte[] convertToByteArray(MediaFile text)
+        {
+            var stream = text.GetStream();
+            BinaryReader binRead = new BinaryReader(stream);
+            return binRead.ReadBytes((int)stream.Length);
         }
     }
 }
