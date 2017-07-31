3d3bb14cfc8fae86193339ba5f3f6a26b1fdd531
Shranal Kumar <skmu316@aucklanduni.ac.nz>
2017-07-31 15:33:08 +1200

    Made selection box

--- PhotoTextTranslator/PhotoTextTranslator/AnalysePhoto.xaml.cs
+++ PhotoTextTranslator/PhotoTextTranslator/AnalysePhoto.xaml.cs
@@ -17,5 +17,17 @@ namespace PhotoTextTranslator
         {
             InitializeComponent();
         }
+
+        private void translateButton_Clicked(object sender, EventArgs e)
+        {
+            picSentence.Text = TranslateManager.createInstance.getSentence();
+        }
+
+        private async void languages_Clicked(object sender, EventArgs e)
+        {
+            var action = await DisplayActionSheet("Select language", "Cancel", "", "Text1", "Text2", "Text3", "Text4", "Text5", "Text6", "Text7", "Text8", "Text9", "Text10", "Text1", "Text2", "Text3", "Text4", "Text5", "Text6", "Text7", "Text8", "Text9", "Text10");
+            if (action != "Cancel") { selected.Text = "Language Selected: " + action; }
+            else { selected.Text = ""; }
+        }
     }
 }
